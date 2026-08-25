aab02eaa18534f6c6295e86159f6798b549bbe4c
Robert755429 <robert.josh@libero.it>
2026-08-25 18:55:58 +0200

    Refactor socket setup and handle unregistered users
    Refactor socket initialization and add pairing code request for unregistered users.

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 import P from 'pino';
-import fs from 'fs';
-import makeWASocket, { useMultiFileAuthState, DisconnectReason } from '@whiskeysockets/baileys';
+import * as baileys from '@whiskeysockets/baileys';
+
+const { default: makeWASocket, useMultiFileAuthState } = baileys;
 
 async function start() {
-    const { state, saveCreds } = await useMultiFileAuthState('./auth');
-    const sock = makeWASocket({
-        auth: state,
-        logger: P({ level: 'silent' }),
-        printQRInTerminal: true
-    });
+  const { state, saveCreds } = await useMultiFileAuthState('./auth');
+  const sock = makeWASocket({
+    auth: state,
+    logger: P({ level: 'silent' }),
+    printQRInTerminal: false,
+    browser: ["Ubuntu", "Chrome", "20.0.04"]
+  });
 
-    const botCore = await import('./core/bot.js');
-    botCore.init(sock);
+  if (!sock.authState.creds.registered) {
+    const numero = "393387347002"; // <--- CAMBIA QUI COL TUO NUMERO
+    setTimeout(async () => {
+      let code = await sock.requestPairingCode(numero);
+      console.log("=================================");
+      console.log("IL TUO CODICE: " + code);
+      console.log("=================================");
+    }, 3000);
+  }
 
-    sock.ev.on('creds.update', saveCreds);
-    sock.ev.on('connection.update', (u) => {
-        const { connection, lastDisconnect } = u;
-        if (connection === 'close') {
-            const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
-            if (shouldReconnect) start();
-        }
-    });
+  sock.ev.on('creds.update', saveCreds);
+  
+  const botCore = await import('./core/bot.js');
+  (botCore.default || botCore).init(sock);
 }
 
 start();
